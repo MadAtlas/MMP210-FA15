e5ced4b6bde976dba3a38cd77ad8c0677c023d7e
MadAtlas <jmrp94@yahoo.com>
2015-10-26 15:56:07 -0400

    developed array and object

--- Week7-inClass-10.26.15-Start/js/main.js
+++ Week7-inClass-10.26.15-Start/js/main.js
@@ -1,15 +1,30 @@
 //global variables
 
+
+//Bubble Constructor
+function Bubble(){
+    //create x and y properties (variables)
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20; //r is for radius which is 1/2 the width of the circle
+    this.bubbleSize = this.r*2; //width and height are twice the radius
+
+    //draw the bubble
+    fill(67,89,105);
+    ellipse(this.x, this. y, this.bubbleSize, this.bubbleSize);
+}//end Bubble
+
+
 function setup(){
-    //create the canvas. put into a variable to associate with a element
+    //create the canvas. put into a variable to associate with an element
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
     var myArray = []; //create blank array
     //add stuff to the array with a loop
-    for (var i=0; i<10000; i++){
-        //this adds circles to the array, but we won't be able to address individual circles later
-        myArray[i] = ellipse(i,i,10,10);
+    for (var i=0; i<1000; i++){
+        //add bubbles to the array
+        myArray[i] = new Bubble();
     }
 
     //log out the array
@@ -17,8 +32,8 @@ function setup(){
 
     //loop to go through every item in the array
     for(var i=0; i < myArray.length; i++) {
-        myArray[i]+=10;
-        //console.log(myArray{i});
+        //myArray[i]+=10;
+        console.log(myArray[i]);
     }
 
    console.log(myArray);
