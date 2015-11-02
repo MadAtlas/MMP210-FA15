21b1594dd2c4b21ec96d06bc73c7c8fcfe596614
MadAtlas <jmrp94@yahoo.com>
2015-11-02 16:18:26 -0500

    Deleted drawWindow folder, changed other folder names and links to them within html files

---
+++ pattern-assignment/js/main.js
@@ -0,0 +1,51 @@
+//global variables
+var myArray = []; //create blank array
+
+//Bubble Constructor
+function Bubble(){
+    //create x and y properties (variables)
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20; //r is for radius which is 1/2 the width of the circle
+    this.bubbleSize = this.r*2; //width and height are twice the radius
+
+    //method: draw the bubble
+    this.drawBubble = function(){
+        ellipse(this.x, this. y, this.bubbleSize, this.bubbleSize);
+    }
+
+    this.changeColor = function(){
+        fill("orange");
+        this.drawBubble();
+    }
+}//end Bubble
+
+function setup(){
+    //create the canvas. put into a variable to associate with an element
+    var cnv = createCanvas(windowWidth,windowHeight);
+    cnv.parent("windows");
+
+    //add stuff to the array with a loop
+    for (var i=0; i<50; i++){
+        //add bubbles to the array
+        myArray[i] = new Bubble();
+        myArray[i].drawBubble();
+    }
+}//end setup
+
+//listen for the mouse presses with the built in p5 mousePressed method
+function mousePressed(){
+    //check that this is working console.log("mouse Pressed");
+
+    //loop through myArray and check if mouse is in each bubble
+    var d;
+    for(var i=0; i < myArray.length; i++){
+        //myArray[i].x myArray[i].r
+        d = dist(mouseX, mouseY, myArray[i].x, myArray[i].y);
+        //check to see if the distance is less that the radius
+        //ie is the mouse in the bubble
+        if(d < myArray[i].r){
+            myArray[i].changeColor(); //call the color change function
+        }
+    }
+}//end mousePressed
