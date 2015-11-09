5b67fe5ad495ed2186b6cdfc1aca9151dc28be20
MadAtlas <jmrp94@yahoo.com>
2015-11-09 16:06:09 -0500

    drawCircles function removed and replaced with a for loop, rect added to drawBullseye, onClick function started

--- pattern-assignment/js/main.js
+++ pattern-assignment/js/main.js
@@ -4,24 +4,38 @@ var strokeColor;
 
 function setup() {
     createCanvas (windowWidth, windowHeight);
-    circleColor = color(243,156,78);
-    strokeColor = color(45,78,90);
 
-    drawCircles();
-}
+    circleColor = color(random(120), random(120), random(120));
+    strokeColor = color(random(250), random(250), random(250));
 
-function drawCircles() {
-    //draws all the circles
-    //for()
-    drawBullseye(random(width),random(height));
+    var x = 0;
+    var y = 0;
+    var gridWidth = width - 110;
+    for (var i = 0; i < 100; i++) {
+        drawBullseye(x,y);
+        //increment of x
+        x += 125; //same as x = x + 125;
+        //check if w is past the grid width
+        if(x > gridWidth){
+            //set x back to the begining
+            x = 0;
+            //move y down
+            y += 125;
+        }//end if
+    }//end for loop
 }
 
 function drawBullseye(startX,startY) {
-    //singles bulleye
+    //single bulleye
     fill(circleColor);
     stroke(strokeColor);
     strokeWeight(3);
     ellipse(startX,startY,100,100);
+
+    fill(circleColor);
+    stroke(strokeColor);
+    rect(startX,startY,130,130);
+
     ellipse(startX,startY,75,75);
     ellipse(startX,startY,50,50);
 
@@ -31,5 +45,4 @@ function drawBullseye(startX,startY) {
 }
 
 function mouseClicked() {
-
 }
