f4f3df9f9dd013bed1e01cb76542033bb1bd0ee8
MadAtlas <jmrp94@yahoo.com>
2015-11-17 02:53:03 -0500

    Hardcoded measurements for most of the lines and shapes, window made and colored

--- window-assignment-Wloop/js/main.js
+++ window-assignment-Wloop/js/main.js
@@ -1,18 +1,17 @@
-//global variables
-var shadowColor;
-
-
 function setup() {
     createCanvas (windowWidth, windowHeight);
-
-    shadowColor = color('rgba(20,20,20,.45)');
 
     drawWindows();
 }
 
 function drawWindows() {
+    //vars
+    partHeight = 140
+    partWidth = 150;
+
     var x = 0;
     var y = 0;
+
     var gridWidth = width - 110;
     for (var i = 0; i < 10; i++) {
         drawOnewindow(x,y);
@@ -31,15 +30,29 @@ function drawWindows() {
 function drawOnewindow(startX,startY) {
     //single window
     //window frame
+    strokeWeight(1);
+    fill(48,167,98);
     rect(startX,startY,200,40);
     rect(startX,startY+340,200,40);
 
     //bottom part of window
-    rect(startX+20,startY+190,160,150)
-    rect(startX+25,startY+195,150,140)
+    strokeWeight(1);
+    fill(98,147,228);
+    rect(startX+20,startY+190,160,150);
+    fill(48,17,98);
+    rect(startX+25,startY+195,150,140);
 
+    strokeWeight(3);
+line(startX+25,startY+265,startX+175,startY+265); line(startX+100,startY+195,startX+100,startY+335);
 
     //top part of window
-    rect(startX+20,startY+40,160,150)
-    rect(startX+25,startY+45,150,140)
+    strokeWeight(1);
+    fill(98,147,228);
+    rect(startX+20,startY+40,160,150);
+    fill(48,17,98);
+    rect(startX+25,startY+45,150,140);
+
+    strokeWeight(3);
+line(startX+25,startY+115,startX+175,startY+115);
+line(startX+100,startY+45,startX+100,startY+185);
 }
